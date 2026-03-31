bdecbab5d1401a5c59cd961ca98dad1a9138fa8f
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-03-31 10:52:27 +0700

    first commit

---
+++ input.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+using namespace std;
+
+int arr[20], B[20];
+int n;
+
+void input()
+{
+    while (true)
+    { 
+        cout << "Masukkan panjang element array: ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nMaksimal panjang array yang dapat dimasukkan adalah 20." << endl;
+        }
+    }
+    cout << endl;
+    cout << "================================================" << endl;
+    cout << "=============Masukkan Element Array=============" << endl;
+    cout << "================================================" << endl;
+    
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << i + 1 << ": ";
+        cin >> arr[i];
+    }
+}
+
+//membuat fungsi mergeSort
+void mergeSort(int low, int high)
+{
+    if (low >= high)
+    {
+       return;
+    }
+
+    int mid = (low + high) / 2;
+    mergeSort(low, mid);
+    mergeSort(mid + 1, high);
+
+    int i = low;
+    int j = mid + 1;
+    int k = low;
+
+    while (i <= mid && j <= high)
+    {
+        if (arr[i] < arr[j])
+        {
+            B[k] = arr[i];
+            i++;
+        }
+        else
+        {
+            B[k] = arr[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i <= mid)
+    {
+        B[k] = arr[i];
+        i++;
+        k++;
+    }
+
+    for (int x = low; x <= high; x++)
+    {
+        arr[x] = B[x];
+    }
+}
+
+void output()
+{
+    cout << "\nData setelah diurutkan m(marge Sort): ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    input();
+    mergeSort(0, n - 1);
+    output();
+    return 0;
+}
