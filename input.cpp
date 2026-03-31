a2fe4e409ae8882c5efd0d1be1b4a3b5c9be9379
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-03-31 11:17:46 +0700

    proses penggabungan

--- input.cpp
+++ input.cpp
@@ -43,9 +43,27 @@ void mergeSort(int low, int high)
     int mid = (low + high) / 2;
     mergeSort(low, mid);
     mergeSort(mid + 1, high);
+
+    int i = low;
+    int j = mid + 1;
+    int k = low;
+
+    while (i <= mid && j <= high)
+    {
+        if (arr[i] < arr[j])
+        {
+            B[k] = arr[i];
+            i++;
+        }
+        else
+        {
+            B[k] = arr[j];
+            j++;
+        }
+        k++;
+    }
 }
 
-int main()
-{
-return 0;
+int main(){
+ return 0;
 }
