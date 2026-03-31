eb7334d8e8c23203af76363973c0b381bb7c9ac7
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-03-31 11:07:47 +0700

    membuat fungsi input

--- input.cpp
+++ input.cpp
@@ -20,8 +20,18 @@ void input()
             cout << "\nMaksimal panjang array yang dapat dimasukkan adalah 20." << endl;
         }
     }
+    cout << endl;
+    cout << "================================================" << endl;
+    cout << "=============Masukkan Element Array=============" << endl;
+    cout << "================================================" << endl;
+    
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << i + 1 << ": ";
+        cin >> arr[i];
+    }
 }
 
-int main(){
+int main() {
     return 0;
 }
