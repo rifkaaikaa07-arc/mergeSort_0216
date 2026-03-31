1c6b63dd28e323d3f5aea398391d88f121f2ef0d
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-03-31 11:05:12 +0700

    menambahkan panjang array

--- input.cpp
+++ input.cpp
@@ -4,6 +4,24 @@ using namespace std;
 int arr[20], B[20];
 int n;
 
+void input()
+{
+    while (true)
+    { 
+        cout << "Masukkan panjang element array: ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nMaksimal panjang array yang dapat dimasukkan adalah 20." << endl;
+        }
+    }
+}
+
 int main(){
     return 0;
 }
